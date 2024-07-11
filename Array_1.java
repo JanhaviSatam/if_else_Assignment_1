a79abbe1460cbd30ea9176edc49d3ff8bf6955e6
JanhaviSatam <162903835+JanhaviSatam@users.noreply.github.com>
2024-07-11 15:12:52 +0530

    Add files via upload

---
+++ Array_1.java
@@ -0,0 +1,64 @@
+package Assignment;
+import java.util.Scanner;
+public class Array_1
+{
+	public static void main(String args[])
+	{
+		
+		int n,i,sum=0;
+		char ch;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Product List Size");
+		
+		n= sc.nextInt();
+		String a[] = new String[n];
+		for(i=1;i<n;i++)
+		{
+			System.out.println("Enter product "+i);
+		     a[i]= sc.next();
+		}
+		int b[]= new int[n]	;	
+		
+		for(i=1;i<n;i++)
+		{
+		System.out.println("Enter "+a[i]+" Price");
+		b[i]=sc.nextInt();
+		}
+		System.out.println("Enter GST");
+		int gst=sc.nextInt();
+		System.out.println("Do you want to add GST [Y/N]");
+		ch = sc.next().charAt(0);
+		
+		if( ch == 'Y'||ch=='y')
+		{
+			for(i=1;i<n;i++)	
+         	{
+         	  System.out.println(+i+"."+a[i]+"="+b[i]+"/-");
+         	  sum=sum+b[i]	;
+         	}
+			int sum_1= ((sum*gst)/100) ;
+			System.out.println("------------------");
+			System.out.println("Total :"+sum);
+			System.out.println("GST :"+gst);
+			System.out.println("------------------");
+			System.out.println("Final Total :"+sum_1);
+		}
+		else if( ch == 'N'||ch=='n')
+        {
+         	for(i=1;i<n;i++)	
+         	{
+         	  System.out.println(+i+"."+a[i]+"="+b[i]+"/-");
+         	  sum=sum+b[i]	;
+         	}
+         	
+			System.out.println("------------------");
+			System.out.println("Total :"+sum);
+			
+		}
+	
+		
+		
+	}
+}
