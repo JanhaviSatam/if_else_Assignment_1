908d029d3ad28c970f3929ff0898451411de815a
JanhaviSatam <162903835+JanhaviSatam@users.noreply.github.com>
2024-09-05 17:06:54 +0530

    Add files via upload

---
+++ Library_Mangement_System.java
@@ -0,0 +1,52 @@
+package Assignment;
+class Book{
+	String title[], author[];
+	int isbn[];
+	
+	boolean avaible=true;
+	void remove_book(int isbn[])
+	{
+		
+	}
+}
+class Member extends Book{
+	String name[],borrowed_books[];
+	int member_id[];
+	void borrow_book(int member_id,int isbn[])
+	{
+		super.isbn;
+	}
+	
+	void return_book(int member_id[],int isbn[])
+	{
+		super.isbn;
+	}
+	
+	void list_borrowed_books(int member_id[])
+{
+
+}
+}
+class Library extends Member{
+	String books[],members[];
+	void add_book(String book[])
+	{
+		
+	}
+	void register_member(String member[]) {
+		
+	}
+	 void list_available_books()
+	 {
+		 
+	 }
+}
+public class Library_Mangement_System {
+       public static void main (String args[])
+           {
+	String books []= { "Cattle","Battle","A boat"};
+     String members[]= {"Ram","Shyam","Rita"};
+     
+           }
+	
+}
